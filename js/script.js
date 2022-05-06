312139228c0459dbc1a1ae1bb85ae1cc0878f2b2
Vlad <1maxsinak@gmail.com>
2022-05-06 18:47:48 +0300

    da-da-da

---
+++ js/script.js
@@ -0,0 +1,45 @@
+"use strict"
+
+if (1) {
+    console.log('ok!');
+} else {
+    console.log('Error');
+}
+
+const num = 11111;
+
+if (num < 49) {
+    console.log('Error');
+} 
+    else if 
+        (num > 100) {
+            console.log('много');
+        } 
+    else {
+            console.log('NORMAL!');
+        }
+
+
+    
+
+const num = 39;
+
+(num === 50) ? console.log('Ok!') : console.log('Errrror');
+
+
+const num = 320;
+
+switch (num) {
+    case 49:    
+        console.log('НЕТ!');
+        break;      
+    case 30:
+        console.log('НЕТ!');
+         break;
+    case 50:
+        console.log('оно!');
+        break;
+    default:
+        console.log('Попробуй ещё');
+        break;
+}
